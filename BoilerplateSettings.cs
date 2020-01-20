0c9e136e016c46e66bfe5127dda3a896fe424822
Victor Milovanov <lost@losttech.software>
2020-01-19 19:58:55 -0800

    BoilerplateSettings implements IBoilerplateSettings

--- BoilerplateSettings.cs
+++ BoilerplateSettings.cs
@@ -1,9 +1,11 @@
 ﻿namespace LostTech.App {
     using LostTech.App.DataBinding;
 
-    public class BoilerplateSettings : NotifyPropertyChangedBase, ICopyable<BoilerplateSettings> {
+    public class BoilerplateSettings : NotifyPropertyChangedBase, IBoilerplateSettings, ICopyable<BoilerplateSettings> {
         string? acceptedTerms = null;
         string? whatsNewVersionSeen = null;
+        bool? reportCrashes;
+        bool? enableTelemetry;
 
         public string? AcceptedTerms {
             get => this.acceptedTerms;
@@ -21,9 +23,27 @@
             }
         }
 
+        public bool? ReportCrashes {
+            get => this.reportCrashes;
+            set {
+                this.reportCrashes = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public bool? EnableTelemetry {
+            get => this.enableTelemetry;
+            set {
+                this.enableTelemetry = value;
+                this.OnPropertyChanged();
+            }
+        }
+
         public BoilerplateSettings Copy() => new BoilerplateSettings {
             AcceptedTerms = this.AcceptedTerms,
             WhatsNewVersionSeen = this.WhatsNewVersionSeen,
+            ReportCrashes = this.ReportCrashes,
+            EnableTelemetry = this.EnableTelemetry,
         };
     }
 }
