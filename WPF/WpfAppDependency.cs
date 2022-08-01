4908e1ac168d92c6712fb79365cfb70ad6ff46ef
Victor Nova <lost@losttech.software>
2022-08-01 15:23:37 -0700

    listed project dependencies in WpfAppDependencies

---
+++ WPF/WpfAppDependency.cs
@@ -0,0 +1,16 @@
+﻿namespace LostTech.App.WPF;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public sealed class WpfAppDependency {
+    public static IEnumerable<AppDependecy> Dependencies { get; } =
+        AppDependecy.Dependencies.Concat(
+            new[] {
+                new AppDependecy("Lost Tech App Boilerplate", uri: "https://github.com/losttech/App.Boilerplate", license: "Apache 2.0"),
+                new AppDependecy($"Lost Tech {nameof(XmlSettings)}", uri: "https://github.com/losttech/App.XmlSettings", license: "Apache 2.0"),
+                new AppDependecy(nameof(DesktopBridge), uri: "https://github.com/microsoft/Windows-AppConsult-Tools-DesktopBridgeHelpers", license: "MIT"),
+                new AppDependecy("Windows Community Toolkit", uri: "https://github.com/CommunityToolkit/WindowsCommunityToolkit", license: "MIT"),
+                new AppDependecy("Visual Studio App Center", uri: "https://azure.microsoft.com/en-us/services/app-center/", license: "MIT"),
+            });
+}
