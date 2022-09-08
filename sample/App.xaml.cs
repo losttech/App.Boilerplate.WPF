602464e3173e9742e0160db842759a300eca346c
Victor Nova <lost@losttech.software>
2022-09-08 15:35:03 -0700

    sample app that demonstrates boilerplate

---
+++ sample/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace LostTech.App;
+
+using System;
+
+public partial class App : BoilerplateApp {
+    public override string AppName => "Sample";
+
+    public override string CompanyName => "Contoso";
+
+    public override TimeSpan HeartbeatInterval => TimeSpan.FromDays(30);
+
+    protected override WhatsNew? WhatsNew => new("Hi", "There") { DetailsUri = new Uri("https://example.com") };
+
+    protected override string? AppCenterSecret => null;
+}
---
+++ sample/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LostTech.App;
+
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        this.InitializeComponent();
+    }
+}
